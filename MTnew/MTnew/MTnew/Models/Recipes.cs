13fe34a0c9b6bb92e0ab08a8d3afb6b786a2a1eb
Theis Røpke <Theisropke@gmail.com>
2018-02-20 13:34:20 +0100

    Model og View

---
+++ MTnew/MTnew/MTnew/Models/Recipes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTnew.Models
+{
+    class Recipes
+    {
+        public int Rid { get; set; }
+        public string Overskrift { get; set; }
+        public int Indhold { get; set; }
+    }
+}
