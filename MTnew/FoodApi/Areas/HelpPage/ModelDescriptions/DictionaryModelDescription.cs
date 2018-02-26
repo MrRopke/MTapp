678362f662eea4d2aac3f5b50f799470f4e91836
Illemann <mads@illemann.dk>
2018-02-26 12:44:59 +0100

    * BundleConfig.cs: Delete foodapi
    * Retter.cs:
    * Web.config:
    * favicon.ico:
    * Global.asax:
    * FoodModel.cs:
    * FoodModel.tt:
    * FoodApi.csproj:
    * FoodModel.edmx:
    * Global.asax.cs:
    * packages.config:
    * Site.css:
    * Web.Debug.config:
    * Web.config:
    * Web.Release.config:
    * respond.js:
    * FoodModel.Context.tt:
    * FoodModel.Context.cs:
    * bootstrap.js:
    * bootstrap.css:
    * FoodModel.Designer.cs:
    * FoodModel.edmx.diagram:
    * respond.min.js:
    * _ViewStart.cshtml:
    * Index.cshtml:
    * bootstrap.min.js:
    * jquery-1.10.2.js:
    * RouteConfig.cs:
    * MTnew.iOS.csproj:
    * Error.cshtml:
    * bootstrap.min.css:
    * FilterConfig.cs:
    * WebApiConfig.cs:
    * AssemblyInfo.cs:
    * ApplicationInsights.config:
    * modernizr-2.6.2.js:
    * HelpPage.css:
    * _Layout.cshtml:
    * jquery-1.10.2.min.js:
    * jquery-1.10.2.min.map:
    * HomeController.cs:
    * ValuesController.cs:
    * Web.config:
    * Api.cshtml:
    * jquery-1.10.2.intellisense.js:
    * Index.cshtml:
    * glyphicons-halflings-regular.eot:
    * glyphicons-halflings-regular.svg:
    * _ViewStart.cshtml:
    * glyphicons-halflings-regular.ttf:
    * glyphicons-halflings-regular.woff:
    * HelpPageApiModel.cs:
    * _Layout.cshtml:
    * XmlDocumentationProvider.cs:
    * ApiDescriptionExtensions.cs:
    * HelpPageAreaRegistration.cs:
    * HelpPageConfig.cs:
    * HelpController.cs:
    * TextSample.cs:
    * ImageSample.cs:
    * ResourceModel.cshtml:
    * InvalidSample.cs:
    * HelpPageConfigurationExtensions.cs:
    * ObjectGenerator.cs:
    * SampleDirection.cs:
    * ModelNameHelper.cs:
    * HelpPageSampleKey.cs:
    * ModelDescription.cs:
    * ModelNameAttribute.cs:
    * ParameterAnnotation.cs:
    * ParameterDescription.cs:
    * EnumValueDescription.cs:
    * Samples.cshtml:
    * ApiGroup.cshtml:
    * HelpPageSampleGenerator.cs:
    * Parameters.cshtml:
    * EnumTypeModelDescription.cs:
    * TextSample.cshtml:
    * ImageSample.cshtml:
    * ModelDescriptionGenerator.cs:
    * DictionaryModelDescription.cs:
    * CollectionModelDescription.cs:
    * SimpleTypeModelDescription.cs:
    * IModelDocumentationProvider.cs:
    * InvalidSample.cshtml:
    * ComplexTypeModelDescription.cs:
    * KeyValuePairModelDescription.cs:
    * HelpPageApiModel.cshtml:
    * ModelDescriptionLink.cshtml:
    * EnumTypeModelDescription.cshtml:
    * DictionaryModelDescription.cshtml:
    * CollectionModelDescription.cshtml:
    * SimpleTypeModelDescription.cshtml:
    * ComplexTypeModelDescription.cshtml:
    * KeyValuePairModelDescription.cshtml:

--- MTnew/FoodApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FoodApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- MTnew/FoodApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FoodApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
